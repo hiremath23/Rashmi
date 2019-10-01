87d1b54accd9639ee0305000c93eac7488479bb5
hiremath23 <56018497+hiremath23@users.noreply.github.com>
2019-10-01 13:12:51 +0530

    Add files via upload

---
+++ add1.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+public class add1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=5;
+    int b=8;
+    int c=a*b;
+    System.out.println(c);
+	}
+
+}
