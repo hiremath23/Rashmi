5a5c01f4b0bfbbe62db8dcaa7fe07838e48dc3ee
hiremath23 <56018497+hiremath23@users.noreply.github.com>
2019-10-03 12:48:27 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner s = new Scanner(System.in);
+		   System.out.println("enter user name");
+		   String username= s.nextLine();
+		   System.out.println("userame is:" +username);
+		   
+	}
+	
+
+}
